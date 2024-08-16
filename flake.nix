939a30bf2ccc465fcb3eefff33b094314a273d7c
random-sir <antoni.torres@outlook.com>
2024-08-15 21:00:47 -0300

    Added inputs to configuration.nix

--- flake.nix
+++ flake.nix
@@ -11,6 +11,9 @@
     {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+        specialArgs = {
+          inherit inputs;
+        };
         modules = [
           # Import the previous configuration.nix we used,
           # so the old configuration file still takes effect
