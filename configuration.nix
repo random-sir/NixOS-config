228fa3a86f068d3da217e01fe2df8a36585406c3
random-sir <antoni.torres@outlook.com>
2024-10-29 09:09:43 -0300

    Enabled auto nix garbage collection (via nh)

--- configuration.nix
+++ configuration.nix
@@ -154,6 +154,8 @@
   #nh, the cool nix helper B-)
   programs.nh = {
     enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
     flake = "/home/random-sir/nixos-config";
   };
 
